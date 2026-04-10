a8370b37f6cd22654fe52a1d886d4b6db4c998dd
Peter Huang <petersuper03@gmail.com>
2026-04-10 21:54:02 +0800

    initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DISCORD_BOT_TOKEN: str
+    BASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+settings = Settings
