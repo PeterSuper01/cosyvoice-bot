886c1394e3eeb2604821b4840a11fa6e831cf398
Peter Huang <petersuper03@gmail.com>
2026-04-12 16:27:57 +0800

    rename tts client

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ intents.message_content = True
 class TTSBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix="!", intents=intents)
-        self.tts = TTSClient()
+        self.tts_client = TTSClient()
 
     async def close(self):
-        await self.tts.close()
+        await self.tts_client.close()
         await super().close()
